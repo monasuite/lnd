e1cba78c69f3711d65264dd0a55f6d0e98227ec6
wakiyamap <wakiyamap@gmail.com>
2020-12-18 02:43:58 +0900

    Remove comment(lint)

--- lnwallet/test/test_interface.go
+++ lnwallet/test/test_interface.go
@@ -30,7 +30,6 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/monaarchives/btcwallet/chain"
 	"github.com/monaarchives/btcwallet/walletdb"
-	// monaarchives bdb
 	_ "github.com/monaarchives/btcwallet/walletdb/bdb"
 	"github.com/monasuite/lnd/chainntnfs"
 	"github.com/monasuite/lnd/chainntnfs/btcdnotify"
