d954189b9cad0418be02efbe8ccf571f59f2e32e
wakiyamap <wakiyamap@gmail.com>
2021-04-28 14:58:46 +0900

    Fix lint errors

--- log.go
+++ log.go
@@ -40,7 +40,6 @@ import (
 	"github.com/monasuite/lnd/watchtower"
 	"github.com/monasuite/lnd/watchtower/wtclient"
 	"github.com/monasuite/neutrino"
-
 )
 
 // replaceableLogger is a thin wrapper around a logger that is used so the
